a5ffb21f271776e47243e9330cc495081bec7663
Arthi-chaud <arthur.jamet@gmail.com>
2022-07-05 10:33:46 +0200

    Front: Define The Color Theme to use

--- front/App.tsx
+++ front/App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ColorValue } from 'react-native';
+import ColorTheme from './Theme';
+
+const ExampleBox = (props: { textColor: ColorValue, backgroundColor: ColorValue }) => (
+  <View style={{ backgroundColor: props.backgroundColor }}>
+    <Text style={{ fontSize: 20, textAlign: 'center', color: props.textColor }} >Hello</Text>
+  </View>
+)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ExampleBox backgroundColor={ColorTheme.primary} textColor={ColorTheme.onPrimary}/>
+      <ExampleBox backgroundColor={ColorTheme.primaryVariant} textColor={ColorTheme.onPrimary}/>
+      <ExampleBox backgroundColor={ColorTheme.secondary} textColor={ColorTheme.onSecondary}/>
+      <ExampleBox backgroundColor={ColorTheme.secondaryVariant} textColor={ColorTheme.onPrimary}/>
+      <ExampleBox backgroundColor={ColorTheme.error} textColor={ColorTheme.onError}/>
+      <ExampleBox backgroundColor={ColorTheme.surface} textColor={ColorTheme.onSurface}/>
+      <ExampleBox backgroundColor={ColorTheme.surface} textColor={ColorTheme.placeholder}/>
+      <ExampleBox backgroundColor={ColorTheme.background} textColor={ColorTheme.onBackground}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ front/Theme.tsx
@@ -0,0 +1,27 @@
+/**
+ * Color theme to use thoughout the application
+ * Using the Material Color guidelines
+ * TODO: integrate with the to-be-determined design framework
+ */
+const ColorTheme = {
+	primary: '#5db075',
+	primaryVariant: '#008000',
+	onPrimary: '#ffffff',
+
+	secondary: '#00bdbd',
+	secondaryVariant: '#008b8c',
+	onSecondary: '#ffffff',
+
+	background: '#F5F5F5',
+	onBackground: '#000000',
+
+	surface: '#F6F6F6',
+	onSurface: '#000000',
+
+	placeholder: '#C9C9C9',
+
+	error: '#B00020',
+	onError: '#FFFFFF',
+};
+
+export default ColorTheme;
