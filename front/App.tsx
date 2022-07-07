e61968f08a4ba03cb82eba0824bc479978fa7222
Arthur Jamet <60505370+Arthi-chaud@users.noreply.github.com>
2022-07-07 14:19:30 +0200

    Front: Define Color theme (#64)

--- front/App.tsx
+++ front/App.tsx
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ColorValue } from 'react-native';
+import ColorTheme from './Theme';
+
+const ExampleBox = (props: { textColor: ColorValue, backgroundColor: ColorValue }) => (
+  <View style={{ backgroundColor: props.backgroundColor }}>
+    <Text style={{ fontSize: 20, textAlign: 'center', color: props.textColor }} >Hello</Text>
+  </View>
+)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ExampleBox backgroundColor={ColorTheme.primary} textColor={ColorTheme.onPrimary}/>
+      <ExampleBox backgroundColor={ColorTheme.primaryVariant} textColor={ColorTheme.onPrimary}/>
+      <ExampleBox backgroundColor={ColorTheme.secondary} textColor={ColorTheme.onSecondary}/>
+      <ExampleBox backgroundColor={ColorTheme.secondaryVariant} textColor={ColorTheme.onPrimary}/>
+      <ExampleBox backgroundColor={ColorTheme.error} textColor={ColorTheme.onError}/>
+      <ExampleBox backgroundColor={ColorTheme.surface} textColor={ColorTheme.onSurface}/>
+      <ExampleBox backgroundColor={ColorTheme.surface} textColor={ColorTheme.placeholder}/>
+      <ExampleBox backgroundColor={ColorTheme.backgroundLight} textColor={ColorTheme.onBackgroundLight}/>
+      <ExampleBox backgroundColor={ColorTheme.backgroundDark} textColor={ColorTheme.onBackgroundDark}/>
+      <View
+        style={{
+          paddingVertical: 20,
+          borderBottomColor: ColorTheme.divider,
+          borderBottomWidth: StyleSheet.hairlineWidth,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: ColorTheme.backgroundDark,
     justifyContent: 'center',
   },
 });
---
+++ front/Theme.tsx
@@ -0,0 +1,31 @@
+/**
+ * Color theme to use thoughout the application
+ * Using the Material Color guidelines
+ * TODO: integrate with the to-be-determined design framework
+ */
+const ColorTheme = {
+	primary: '#5db075',
+	primaryVariant: '#008000',
+	onPrimary: '#ffffff',
+
+	secondary: '#00bdbd',
+	secondaryVariant: '#008b8c',
+	onSecondary: '#ffffff',
+
+	backgroundLight: '#F0F0F0',
+	onBackgroundLight: '#000000',
+	backgroundDark: '#292929',
+	onBackgroundDark: '#FFFFFF',
+
+	surface: '#F6F6F6',
+	onSurface: '#000000',
+
+	placeholder: '#C9C9C9',
+
+	error: '#B00020',
+	onError: '#FFFFFF',
+
+	divider: "#DDDDDD",
+};
+
+export default ColorTheme;
