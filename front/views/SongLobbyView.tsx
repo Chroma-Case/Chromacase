62f1576a70f6620f2c339dc89ed5c882b9518df7
Arthi-chaud <arthur.jamet@gmail.com>
2022-09-03 09:42:26 +0200

    Front: Song Lobby Page: Basic Layout with song title, illustration and illustration

--- front/views/SongLobbyView.tsx
+++ front/views/SongLobbyView.tsx
@@ -1,9 +1,11 @@
 import { useRoute } from "@react-navigation/native";
-import { View } from "react-native"
-import { Text } from "react-native-paper";
+import { Image, View } from "react-native"
+import { Surface, Text } from "react-native-paper";
 import API from "../API";
 import { useQuery } from 'react-query';
 import LoadingComponent from "../components/loading";
+import React, { useEffect } from "react";
+import logo from '../assets/cover.png';
 
 interface SongLobbyProps {
 	// The unique identifier to find a song
@@ -13,17 +15,29 @@ interface SongLobbyProps {
 const SongLobbyView = () => {
 	const route = useRoute();
 	const props: SongLobbyProps = route.params as any;
-	const { isLoading, isError, data } = useQuery(['song', props.songId], API.getSong(props.songId))
+	const { isLoading, data } = useQuery(['song', props.songId], () => API.getSong(props.songId));
+	useEffect(() => {}, [isLoading]);
 	if (isLoading)
 		return <View style={{ flexGrow: 1, justifyContent: 'center' }}>
 			<LoadingComponent/>
 		</View>
-	return <View>
-		
-		<Text>
-			{props.songId}
-		</Text>
-	</View>
+	return (
+		<View style={{ padding: 30 }}>
+			<View style={{ flexDirection: 'row' }}>
+				<View style={{ flex: 3 }}>
+					<Surface style={{ aspectRatio: 1, zIndex: 0 }}>
+						<Image source={logo} style={{ height: '100%', width: undefined, resizeMode: 'contain' }}/>
+					</Surface>
+				</View>
+				<View style={{ flex: 3, padding: 10 }}>
+					<Text>{data.title}</Text>
+					<Text></Text>
+				</View>
+			</View>
+			<View style={{ paddingVertical: 10 }}/>
+			<Text>{data.description}</Text>
+		</View>
+	)
 }
 
 export default SongLobbyView;
