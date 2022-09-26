ad002e73538afea8e16fdd853be1023a3c1b1b0c
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2022-09-26 12:26:56 +0200

    [MOV] files

---
+++ front/views/HomeView/HomeView.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../../state/Store';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+
+import HomeView from './HomeView';
+import { en, fr } from '../../i18n/Translations';
+
+describe('<HomeView />', () => {
+  const view = <Provider store={store}><HomeView /></Provider>;
+
+  beforeEach(() => render(view));
+
+  it('has should display the text in default language', async () => {
+    expect((await screen.findAllByText(en.signoutBtn)).length).toBe(1);
+  });
+
+  it('has should display the text in the new language', async () => {
+    fireEvent.press(screen.getByText('Change language'));
+    expect(store.getState().language.value).toBe('fr');
+    screen.update(view);
+    expect((await screen.findAllByText(fr.signoutBtn)).length).toBe(1);
+  });
+});
