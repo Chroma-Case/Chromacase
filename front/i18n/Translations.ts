3686cb0211fc2dc1ea8cd30b9a6dcac3e487e5df
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2022-09-26 12:25:04 +0200

    [UPD] added translations for the skills and added translation to the buttons

--- front/i18n/Translations.ts
+++ front/i18n/Translations.ts
@@ -1,27 +1,48 @@
 export const en = {
 	welcome: 'Welcome',
+	welcomeMessage: 'Welcome back ',
 	signoutBtn: 'Sign out',
 	signinBtn: 'Sign in',
 	changeLanguageBtn: "Change language",
 	searchBtn: "Search",
 	playBtn: 'Play',
+	songPageBtn: 'Go to song page',
 	level: 'Level',
 	chapters: 'Chapters',
 	bestScore: 'Best Score',
 	lastScore: 'Last Score',
-	play: 'Play'
+	play: 'Play',
+
+	// competencies
+	pedalsCompetency: 'Pedals',
+	rightHandCompetency: 'Right hand',
+	leftHandCompetency: 'Left hand',
+	accuracyCompetency: 'Accuracy',
+	arpegeCompetency: 'Arpeges',
+	chordsCompetency: 'Chords',
+
 };
 
 export const fr: typeof en = {
 	welcome: 'Bienvenue',
+	welcomeMessage: 'Re-Bonjour ',
 	signoutBtn: 'Se déconnecter',
 	signinBtn: 'Se connecter',
 	changeLanguageBtn: "Changer la langue",
 	searchBtn: "Rechercher",
 	playBtn: 'Jouer',
+	songPageBtn: 'Aller sur la page de la chanson',
 	level: 'Niveau',
 	chapters: 'Chapitres',
 	bestScore: 'Meilleur Score',
 	lastScore: 'Dernier Score',
-	play: 'Jouer'
+	play: 'Jouer',
+
+	// competencies
+	pedalsCompetency: 'Pédales',
+	rightHandCompetency: 'Main droite',
+	leftHandCompetency: 'Main gauche',
+	accuracyCompetency: 'Précision',
+	arpegeCompetency: 'Arpèges',
+	chordsCompetency: 'Accords',
 };
