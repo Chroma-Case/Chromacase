acee94f42f394d7da2b4fe59ba0c8898fc675853
Clément Le Bihan <clement.lebihan773@gmail.com>
2022-10-05 12:13:32 +0200

    testing formik yup in react native

--- front/views/AuthenticationView.tsx
+++ front/views/AuthenticationView.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import { useDispatch } from '../state/Store';
 import { translate } from "../i18n/i18n";
+<<<<<<< HEAD
 import { setUserToken } from "../state/UserSlice";
 import { Center, Button, Text } from 'native-base';
+=======
+import { setUserToken, unsetUserToken } from "../state/UserSlice";
+
+// create a view that can login and sign up
+
+const AuthenticationView = () => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ textAlign: "center" }}>{ t('welcome') }</Text>
+      <Button onPress={() => dispatch(setUserToken('token'))}>{ translate('signinBtn') }</Button>
+      <Button onPress={() => dispatch(unsetUserToken())}>{ translate('signupBtn') }</Button>
+      <Button onPress={() => dispatch(unsetUserToken())}>{ translate('signoutBtn') }</Button>
+    </View>
+  );
+}
+
+/*
+>>>>>>> 46d6d0a (testing formik yup in react native)
 
 const AuthenticationView = () => {
 	const dispatch = useDispatch();
@@ -15,6 +36,7 @@ const AuthenticationView = () => {
 		</Center>
 	);
 }
+*/
 
 
 export default AuthenticationView;
