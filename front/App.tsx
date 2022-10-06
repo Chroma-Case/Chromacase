599e785969ef4143451bb3b99acc891ad9dce743
Arthi-chaud <arthur.jamet@gmail.com>
2022-10-06 20:06:49 +0100

    Front: Add Theme Provider at root of app

--- front/App.tsx
+++ front/App.tsx
@@ -1,4 +1,3 @@
-import { Provider as PaperProvider } from 'react-native-paper';
 import Theme from './Theme';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -6,16 +5,18 @@ import { Provider } from 'react-redux';
 import store from './state/Store';
 import { Router } from './Navigation';
 import './i18n/i18n';
+import { NativeBaseProvider, extendTheme } from "native-base";
+
 const queryClient = new QueryClient();
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <PaperProvider theme={Theme}>
-          <Router/>
-        </PaperProvider>
-      </QueryClientProvider>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<QueryClientProvider client={queryClient}>
+				<NativeBaseProvider theme={Theme}>
+					<Router />
+				</NativeBaseProvider>
+			</QueryClientProvider>
+		</Provider>
+	);
 }
