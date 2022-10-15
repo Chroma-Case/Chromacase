5aae18302a1653f4c5f09a61f7248b5db32b4d35
Clément Le Bihan <clement.lebihan773@gmail.com>
2022-10-16 00:54:23 +0200

    added missing translations and a temporary implementation of the AuthentificationWiew

--- front/views/AuthenticationView.tsx
+++ front/views/AuthenticationView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from '../state/Store';
 import { translate } from "../i18n/i18n";
 import { setUserToken } from "../state/UserSlice";
-import { Center, Button, Text } from 'native-base';
+import { Center, Button, Text, Box } from 'native-base';
 import SigninForm from "../components/forms/signinform";
 import SignupForm from "../components/forms/signupform";
 
@@ -19,16 +19,39 @@ const checkLoginCredentials = async (username: string, password: string, tokenSe
 	});
 };
 
+const checkSignupCredentials = async (username: string, password: string, email: string, tokenSetter: (t: string) => void) => {
+	return new Promise<string>((resolve, reject) => {
+		setTimeout(() => {
+			if (username === "bluub") {
+				return reject("bluub is a cool username but déjà prit");
+			}
+			setTimeout(() => tokenSetter("4321"), 100);
+			return resolve(translate("accountCreated"));
+		}, 1000);
+	});
+};
+
 const AuthenticationView = () => {
 	const dispatch = useDispatch();
+	const [mode, setMode] = React.useState("signin" as "signin" | "signup");
 
 	return (
 		<Center style={{ flex: 1 }}>
 			<Text>{translate('welcome')}</Text>
-      <Text>username, password:</Text>
-      <Text>katerina, 1234</Text>
-      <SigninForm onSubmit={(u, p) => checkLoginCredentials(u, p, (t) => dispatch(setUserToken(t)))} />
-      <Button variant="ghost" colorScheme="secondary" >{translate("forgottenPassword")}</Button>
+			{mode === "signin" ? (
+				<Box>
+					<Text>username, password:</Text>
+					<Text>katerina, 1234</Text>
+					<SigninForm onSubmit={(username, password) => checkLoginCredentials(username, password, (token) => dispatch(setUserToken(token)))} />
+					<Button variant="ghost" colorScheme="secondary" >{translate("forgottenPassword")}</Button>
+				</Box>
+
+			) : (
+				<SignupForm onSubmit={(username, password, email) => checkSignupCredentials(username, password, email, (token) => dispatch(setUserToken(token)))} />
+			)}
+			<Button onPress={() => setMode(mode === "signin" ? "signup" : "signin")}>
+				<Text>{mode === "signin" ? translate("signUp") : translate("signIn")}</Text>
+			</Button>
 		</Center>
 	);
 };
