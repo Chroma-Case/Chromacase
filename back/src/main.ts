f8a58ec5f640ab237b9884c4afd7f6851999ee38
Zoe Roux <zoe.roux@sdg.moe>
2022-10-25 02:15:19 +0900

    Add populate script and artist api

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
 
+	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(3000);
 }
 bootstrap();
