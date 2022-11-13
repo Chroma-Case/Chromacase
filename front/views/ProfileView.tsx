7c99b108e6542d8e66808f0736ead46d18fc1e4b
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2022-11-13 08:40:35 +0100

    [ADD] profile page

---
+++ front/views/ProfileView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+const ProfileView = () => {
+    return (<View>
+            <Text>Profile</Text>
+        </View>);
+}
+
+export default ProfileView;
