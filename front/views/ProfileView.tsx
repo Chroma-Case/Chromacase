1d863a11366816d591033828e0f02b03c0efd9b0
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2022-11-15 13:52:30 +0100

    [WIP] added all the elements to the profile view

--- front/views/ProfileView.tsx
+++ front/views/ProfileView.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Box, Image, Heading, HStack, Card, Text } from 'native-base';
 
 const ProfileView = () => {
-    return (<View>
-            <Text>Profile</Text>
-        </View>);
+    return (
+    <View style={{flexDirection: 'column'}}>
+        <View style={{flexDirection: 'row'}}>
+            <Box w="20%" paddingRight={2} paddingLeft={5} paddingY={2}>
+	    		<Image borderRadius={100} source={{
+          			   uri: "https://wallpaperaccess.com/full/317501.jpg" // TODO : put the actual profile pic
+        			}} alt="Profile picture" size="lg"
+	    		/>
+	    	</Box>
+            <Box w="80%" paddingY={10}>
+                <Heading>User</Heading>
+            </Box>
+        </View>
+
+        <Card>
+            <Heading>Medals</Heading>
+            <HStack alignItems={'row'} space='5'>
+                <Image source={{
+                    uri: "https://wallpaperaccess.com/full/317501.jpg" // TODO : put the actual profile pic
+                }} alt="Profile picture" size="lg"
+                />
+                <Image source={{
+                    uri: "https://wallpaperaccess.com/full/317501.jpg" // TODO : put the actual profile pic
+                }} alt="Profile picture" size="lg"
+                />
+                <Image source={{
+                    uri: "https://wallpaperaccess.com/full/317501.jpg" // TODO : put the actual profile pic
+                }} alt="Profile picture" size="lg"
+                />
+                <Image source={{
+                    uri: "https://wallpaperaccess.com/full/317501.jpg" // TODO : put the actual profile pic
+                }} alt="Profile picture" size="lg"
+                />
+            </HStack>
+        </Card>
+            
+        <Card>
+            <Heading>My stats</Heading>
+            <Text>Most played song : ...</Text>
+            <Text>Good notes played : ...</Text>
+            <Text>Longest combo : ...</Text>
+            <Text>Favorite genre : ...</Text>
+        </Card>
+    </View>
+    );
 }
 
 export default ProfileView;
