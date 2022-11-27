6312fc33371293a351dab3cc9130be3f879d12c3
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2022-11-27 09:27:21 +0100

    [FIX] splashscreen

--- front/app.config.ts
+++ front/app.config.ts
@@ -6,7 +6,7 @@ module.exports = {
   "icon": "./assets/icon.png",
   "userInterfaceStyle": "light",
   "splash": {
-    "image": "./assets/cover.png",
+    "image": "./assets/splashLogo.png",
     "resizeMode": "contain",
     "backgroundColor": "#ffffff"
   },
