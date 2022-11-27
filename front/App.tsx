bc0b412f1e65b25376d9b956a0d36c7fb7f3061e
Chloé Chauvin <60068251+Nekory23@users.noreply.github.com>
2022-11-27 17:20:23 +0900

    54-écran-de-titre (#109)
    * [ADD] function to hide the splash screen
    
    * [DEL] useless lines
    
    * [ADD] compatibility
    
    * [UPD] settings for the splash screen
    
    * [ADD] chromacase logo on the splash screen
    
    * [UPD] splash logo
    
    * [UPD] set the timeout to 0
    
    * [UPD] set the timeout to 500
    
    * User Slice: Use API Instance instead of access token
    
    * Front: Fix API calls with JWT Token
    
    * Front: Handle Empty Server response
    
    * Front: Fix User Model
    
    * Front: Signin Form: Fix submittingstate on error
    
    * Front: Remove logs
    
    * Front/translate refactor (#110)
    
    * Front: i18n: Create component
    
    * Front: Use new translation component
    
    * Front: Translation COmpoent: Change props name
    
    * Front: Fix merge
    
    * Front: settings persistance (#108)
    
    * Front: Add peristance dependencies
    
    * Front: Fix Cross-platform persistance
    
    * Front: Create Settings Slice
    
    * Front: Use Redux State for settings
    
    * Front: Check if access token is still valid
    
    * Front: Create Language Gate to set correct language at startup
    
    * Front: BEtter handling of Access Token validity
    
    * [MERGE]
    
    * [DEL] useless lines
    
    * [UPD] settings for the splash screen
    
    * [UPD] set the timeout to 0
    
    * [DEL] duplicated line
    
    * [REVERT]
    
    Co-authored-by: Arthi-chaud <arthur.jamet@gmail.com>
    Co-authored-by: Arthur Jamet <60505370+Arthi-chaud@users.noreply.github.com>

--- front/App.tsx
+++ front/App.tsx
@@ -6,12 +6,17 @@ import { Provider } from 'react-redux';
 import store, { persistor } from './state/Store';
 import { Router } from './Navigation';
 import './i18n/i18n';
+import * as SplashScreen from 'expo-splash-screen';
 import { PersistGate } from "redux-persist/integration/react";
 import LanguageGate from "./i18n/LanguageGate";
 
 const queryClient = new QueryClient();
 
 export default function App() {
+
+	SplashScreen.preventAutoHideAsync();
+	setTimeout(SplashScreen.hideAsync, 500);
+
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
