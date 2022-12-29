c7608f292f434d9bd45f3766c01a2164225b55d4
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2022-12-29 09:35:17 +0100

    [ADD] banner and level

--- front/views/ProfileView.tsx
+++ front/views/ProfileView.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Box, Image, Heading, HStack, Card, Button, Spacer } from 'native-base';
+import { Dimensions, View } from 'react-native';
+import { Box, Image, Heading, HStack, Card, Button, Spacer, Text } from 'native-base';
 import Translate from '../components/Translate';
 import { useNavigation } from '@react-navigation/native';
 
 const UserMedals = () => {
     return (
         <Card marginX={20} marginY={10}>
-            <Translate translationKey='medals'/>
+            <Heading>
+                <Translate translationKey='medals'/>
+            </Heading>
             <HStack alignItems={'row'} space='10'>
                 <Image source={{
                     uri: "https://wallpaperaccess.com/full/317501.jpg"
@@ -31,26 +33,46 @@ const UserMedals = () => {
 }
 
 const PlayerStats = () => {
+    const answer = "Answer from back";
+
     return(
         <Card marginX={20} marginY={10}>
-            <Translate translationKey='playerStats'/>
-            <Translate translationKey='mostPlayedSong'/>
-            <Translate translationKey='goodNotesPlayed'/>
-            <Translate translationKey='longestCombo'/>
-            <Translate translationKey='favoriteGenre'/>
+            <Heading>   <Translate translationKey='playerStats'/>             </Heading>
+            <Text>      <Translate translationKey='mostPlayedSong'/> {answer}    </Text>
+            <Text>      <Translate translationKey='goodNotesPlayed'/> {answer}   </Text>
+            <Text>      <Translate translationKey='longestCombo'/> {answer}      </Text>
+            <Text>      <Translate translationKey='favoriteGenre'/> {answer}     </Text>
         </Card>
+
     );
 }
 
-const ProfilePicture = () => {
+const ProfilePictureBannerAndLevel = () => {
+    const profilePic = "https://wallpaperaccess.com/full/317501.jpg"
+    const username = "Username"
+    const level = "1"
+
+    // banner size
+    const dimensions = Dimensions.get('window');
+    const imageHeight = dimensions.height / 5;
+    const imageWidth = dimensions.width;
+
+    // need to change the padding for the username and level
+
     return (
-        <View style={{flexDirection: 'row', marginHorizontal: 30, marginVertical: 10}}>
-	    	<Image borderRadius={100} source={{
-          		   uri: "https://wallpaperaccess.com/full/317501.jpg" // TODO : put the actual profile pic
-        		}} alt="Profile picture" size="lg"
-	    	/>
-            <Box w="100%" paddingY={10} paddingLeft={5}>
-            <Heading>Username</Heading>
+        <View style={{flexDirection: 'row'}}>
+            <Image source={{ uri : "https://wallpaperaccess.com/full/317501.jpg" }} size="lg" 
+                   style={{ height: imageHeight, width: imageWidth, zIndex:0, opacity: 0.5 }}
+            />
+            <Box zIndex={1} position={"absolute"} marginY={10} marginX={10}>
+                <Image borderRadius={100} source={{ uri: profilePic }} 
+                       alt="Profile picture" size="lg" 
+                       style= {{position: 'absolute'}}
+	            />
+                <Box w="100%" paddingY={3} paddingLeft={100}>
+                    <Heading>{username}</Heading>
+                    <Heading>Level : {level}</Heading>
+                </Box>
             </Box>
         </View>
     );
@@ -61,17 +83,14 @@ const ProfileView = () => {
 
     return (
     <View style={{flexDirection: 'column'}}>
-        <View style={{flexDirection: 'row'}}>
-            <ProfilePicture/>
-            <Spacer/>
-            <Box w="10%" paddingY={10} paddingLeft={5} paddingRight={50}>
-                <Button onPress={() => navigation.navigate('Settings')} style={{margin: 10}}>
-                    <Translate translationKey='settingsBtn'/>
-                </Button>
-            </Box>
-        </View>
+        <ProfilePictureBannerAndLevel/>
         <UserMedals/>
         <PlayerStats/>
+        <Box w="10%" paddingY={10} paddingLeft={5} paddingRight={50} zIndex={1}>
+            <Button onPress={() => navigation.navigate('Settings')} style={{margin: 10}}>
+                <Translate translationKey='settingsBtn'/>
+            </Button>
+        </Box>
     </View>
     );
 }
