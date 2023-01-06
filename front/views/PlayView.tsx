b55ea59733b9fa5227b0437fca228da712756f15
Arthi-chaud <arthur.jamet@gmail.com>
2023-01-06 15:42:16 +0000

    Play Page: Get Song id by props + get midi events

--- front/views/PlayView.tsx
+++ front/views/PlayView.tsx
@@ -4,20 +4,50 @@ import * as ScreenOrientation from 'expo-screen-orientation';
 import {  Box, Center, Column, IconButton, Progress, Row, Toast, View } from 'native-base';
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native';
+import { useQuery } from 'react-query';
+import API from '../API';
+import LoadingComponent from '../components/Loading';
 
-function onMIDISuccess(midiAccess) {
-	Toast.show({ description: `MIDI ready!` });
-}
-  
-function onMIDIFailure(msg) {
-	Toast.show({ description: `Failed to get MIDI access - ${msg}` });
-}
 
 class PlayView extends Component {
+	constructor(props: any) {
+		super(props);
+
+		this.onMIDISuccess = this.onMIDISuccess.bind(this);
+	}
+
+	private onMIDISuccess(access) {
+		const inputs = access.inputs;
+		
+		if (inputs.size > 1) {
+			Toast.show({ description: `MIDI ready!`, placement: 'top' });
+		} else {
+			return;
+		}
+		let inputIndex = 0;
+		inputs.forEach((input) => {
+			if (inputIndex != 0) {
+				return;
+			}
+			input.onmidimessage = (message) => {
+				const keyIsPressed = message.data[2] == 100;
+				const keyCode = message.data[1];
+				const eventTime = Date.now();
+				const intensity = null;
+				Toast.show({ description: [keyIsPressed, keyCode, eventTime].toString() });
+			}
+			inputIndex++;
+		});
+	}
+	  
+	private onMIDIFailure(msg) {
+		Toast.show({ description: `Failed to get MIDI access` });
+	}
+	
 
 	override componentDidMount(): void {
 		ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE).catch(() => {});
-		navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+		navigator.requestMIDIAccess().then(this.onMIDISuccess, this.onMIDIFailure);
 	}
 
 	override componentWillUnmount(): void {
@@ -25,9 +55,16 @@ class PlayView extends Component {
 	}
 	override render() {
 		const score = 20;
+
+		if (!this.props.songQuery.data) {
+			return <Center style={{ flexGrow: 1 }}>
+				<LoadingComponent/>
+			</Center>
+		}
 		return (
 			<SafeAreaView style={{ flexGrow: 1, flexDirection: 'column' }}>
 				<View style={{ flexGrow: 1 }}>
+					<Text>{this.state.tick}</Text>
 				</View>
 				<Box shadow={4} style={{ height: '12%', width:'100%', borderWidth: 0.5, margin: 5 }}>
 					<Row justifyContent='space-between' style={{ flexGrow: 1, alignItems: 'center' }} >
@@ -60,7 +97,8 @@ class PlayView extends Component {
 	}
 }
 
-export default function () {
+export default function (props: any) {
 	const navigation = useNavigation();
-	return <PlayView navigation={navigation}/>;
+	const song = useQuery(['song'], () => API.getSong(props.songId));
+	return <PlayView navigation={navigation} songQuery={song}/>;
 };
