6856b2ec87c1b2f321bf3980a833000b9f912322
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2023-01-09 08:00:28 +0100

    [WIP] finished the aesthetic but not functionnal

--- front/components/forms/changePasswordForm.tsx
+++ front/components/forms/changePasswordForm.tsx
@@ -13,25 +13,151 @@ import {
 
 
 interface ChangePasswordFormProps {
-	onSubmit: (oldPassword: string, newPassword: string) => Promise<string>;
+	onSubmit: (
+		oldPassword: string,
+		newPassword: string
+	) => Promise<string>;
 }
-
 
 const ChangePasswordForm = ({ onSubmit }: ChangePasswordFormProps) => {
 	const [formData, setFormData] = React.useState({
-		username: {
+		oldPassword: {
+			value: "",
+			error: null as string | null,
+		},
+		newPassword: {
+			value: "",
+			error: null as string | null,
+		},
+		confirmNewPassword: {
 			value: "",
 			error: null as string | null,
 		},
 	});
+	const [submittingForm, setSubmittingForm] = React.useState(false);
+
+	const validationSchemas = {
+		password: string()
+			.min(4, translate("passwordTooShort"))
+			.max(100, translate("passwordTooLong"))
+			.matches(
+				/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+				translate(
+					"Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+				)
+			)
+			.required("Password is required"),
+	};
+	const toast = useToast();
 
     return (
         <Box>
             <Stack mx="4" style={{ width: '80%', maxWidth: 400 }}>
+				<FormControl
+					isRequired
+					isInvalid={
+						formData.oldPassword.error !== null || 
+						formData.newPassword.error !== null || 
+						formData.confirmNewPassword.error !== null}
+				>
 
+				<FormControl.Label>{translate("oldPassword")}</FormControl.Label>
+				<Input
+					isRequired
+					type="password"
+					placeholder={translate("oldPassword")}
+					value={formData.oldPassword.value}
+					onChangeText={(t) => {
+						let error: null | string = null;
+						validationSchemas.password
+							.validate(t)
+							.catch((e) => (error = e.message))
+							.finally(() => {
+								setFormData({ ...formData, oldPassword: { value: t, error } });
+							});
+					}}
+				/>
+				<FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />} >
+					{formData.oldPassword.error}
+				</FormControl.ErrorMessage>
+
+				<FormControl.Label>{translate("newPassword")}</FormControl.Label>
+				<Input
+					isRequired
+					type="password"
+					placeholder={translate("newPassword")}
+					value={formData.newPassword.value}
+					onChangeText={(t) => {
+						let error: null | string = null;
+						validationSchemas.password
+							.validate(t)
+							.catch((e) => (error = e.message))
+							.finally(() => {
+								setFormData({ ...formData, newPassword: { value: t, error } });
+							});
+					}}
+				/>
+				<FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />} >
+					{formData.newPassword.error}
+				</FormControl.ErrorMessage>
+
+				<FormControl.Label>{translate("confirmNewPassword")}</FormControl.Label>
+				<Input
+					isRequired
+					type="password"
+					placeholder={translate("confirmNewPassword")}
+					value={formData.confirmNewPassword.value}
+					onChangeText={(t) => {
+						let error: null | string = null;
+						validationSchemas.password
+							.validate(t)
+							.catch((e) => (error = e.message))
+							if (!error && t !== formData.newPassword.value) {
+								error = translate("passwordsDontMatch");
+							}
+							setFormData({
+								...formData,
+								confirmNewPassword: { value: t, error },
+							});
+					}}
+				/>
+				<FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />} >
+					{formData.confirmNewPassword.error}
+				</FormControl.ErrorMessage>
+
+				<Button
+					style={{ marginTop: 10 }}
+					isLoading={submittingForm}
+					isDisabled={
+						formData.oldPassword.error !== null ||
+						formData.newPassword.error !== null ||
+						formData.confirmNewPassword.error !== null ||
+						formData.oldPassword.value === "" ||
+						formData.newPassword.value === "" ||
+						formData.confirmNewPassword.value === ""
+					}
+					onPress={async () => {
+						setSubmittingForm(true);
+						try {
+							const resp = await onSubmit(
+								formData.oldPassword.value,
+								formData.newPassword.value
+							);
+							toast.show({ description: resp });
+						} catch (e) {
+							toast.show({ description: e as string });
+						} finally {
+							setSubmittingForm(false);
+						}
+					}}
+				>
+				{translate("submitBtn")}
+				</Button>
+
+				</FormControl>
             </Stack>
         </Box>
     );
 }
 
-export default ChangePasswordForm
+export default ChangePasswordForm;
