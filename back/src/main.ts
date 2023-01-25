d288589e0514d540fb944a5899836169af34af91
danis <amaury.danis-cousandier@epitech.eu>
2023-01-25 17:37:10 +0300

    #94 - functionnal screens

--- back/src/main.ts
+++ back/src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
 	SwaggerModule.setup('api', app, document);
 
 	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors();
 	await app.listen(3000);
 }
 bootstrap();
