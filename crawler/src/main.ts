948b1011e8eaa24a0b1f18104a6f7da8daa4a934
Bluub <46396559+GitBluub@users.noreply.github.com>
2023-01-25 21:28:24 +0900

    Music Crawler (#117)

---
+++ crawler/src/main.ts
@@ -0,0 +1,19 @@
+import { PlaywrightCrawler, Dataset } from 'crawlee';
+import { router } from './routes.js';
+// PlaywrightCrawler crawls the web using a headless
+// browser controlled by the Playwright library.
+const crawler = new PlaywrightCrawler({
+	launchContext: {
+		userDataDir: "/home/bluub/.config/chromium",
+	},
+	maxConcurrency: 1,
+	requestHandler: router,
+	// This function is called if the page processing failed more than maxRequestRetries+1 times.
+	failedRequestHandler({ request, log }) {
+		log.info(`Request ${request.url} failed too many times.`);
+	},
+//	headless: false,
+});
+
+// Add first URL to the queue and start the crawl.
+await crawler.run(['https://musescore.com/sheetmusic?license=to_modify_commercially%2Cto_use_commercially&recording_type=public-domain']);
