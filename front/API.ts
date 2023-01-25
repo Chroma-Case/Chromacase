88c56741fdf52f2501564d16a335c4cbade759af
danis <amaury.danis-cousandier@epitech.eu>
2023-01-25 17:39:23 +0300

    clean code

--- front/API.ts
+++ front/API.ts
@@ -240,7 +240,6 @@ export default class API {
 				try {
 					if (dataKey == 'email') {
 						this.getRequest('http://localhost:3000/users/24').then((res) => {
-							console.table(res);
 							console.log(oldValue, newValue);
 							if (res.email === oldValue) {
 								this.patchRequest('http://localhost:3000/users/24', {'email': newValue}).finally(() => {
@@ -250,7 +249,6 @@ export default class API {
 						});
 					} else if (dataKey == 'password') {
 						this.getRequest('http://localhost:3000/users/24').then((res) => {
-							console.table(res);
 							if (res.password == oldValue) {
 								this.patchRequest('http://localhost:3000/users/24', {'password': newValue}).finally(() => {
 									return resolve('password successfully changed');
