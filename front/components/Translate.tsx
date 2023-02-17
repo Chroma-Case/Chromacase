9a44332d8d9b59b31cc541278854d01ad32fb940
Arthur Jamet <arthur.jamet@gmail.com>
2023-02-17 17:40:44 +0000

    Front: Remove Debug toasts

--- front/components/Translate.tsx
+++ front/components/Translate.tsx
@@ -2,7 +2,6 @@ import { Text, Toast, useTheme } from "native-base";
 import { translate } from "../i18n/i18n";
 import { en } from "../i18n/Translations";
 import { RootState, useSelector } from "../state/Store";
-import { useEffect } from "react";
 
 type TranslateProps = {
 	translationKey: keyof typeof en;
@@ -16,11 +15,7 @@ type TranslateProps = {
 const Translate = ({ translationKey, format }: TranslateProps) => {
 	const selectedLanguage = useSelector((state: RootState) => state.language.value);
 	const translated = translate(translationKey, selectedLanguage);
-	const theme = useTheme();
 
-	useEffect(() => {
-		Toast.show({ description: theme.colors.text[900] });
-	}, [theme]);
 	return <Text>{format ? format(translated) : translated}</Text>;
 }
 
