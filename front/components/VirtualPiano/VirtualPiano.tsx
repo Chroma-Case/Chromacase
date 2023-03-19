d0419cf43eab6eaed4b754381362558a37e23401
Clément Le Bihan <clement.lebihan773@gmail.com>
2023-03-20 00:40:01 +0100

    removed accidental as it's own type and integrated it with note type

--- front/components/VirtualPiano/VirtualPiano.tsx
+++ front/components/VirtualPiano/VirtualPiano.tsx
@@ -1,7 +1,13 @@
 import { Row, Box } from "native-base";
 import React, { useState, useEffect } from "react";
 import Octave from "./Octave";
-import { Note, PianoKey, NoteNameBehavior, KeyPressStyle, keyToStr } from "../../models/Piano";
+import {
+	Note,
+	PianoKey,
+	NoteNameBehavior,
+	KeyPressStyle,
+	keyToStr,
+} from "../../models/Piano";
 
 type VirtualPianoProps = Parameters<typeof Row>[0] & {
 	onNoteDown: (note: PianoKey) => void;
@@ -21,71 +27,74 @@ type VirtualPianoProps = Parameters<typeof Row>[0] & {
 };
 
 const VirtualPiano = ({
-    onNoteDown,
-    onNoteUp,
-    startOctave,
-    startNote,
-    endOctave,
-    endNote,
-    showNoteNames,
-    highlightedNotes,
-    highlightColor,
-    specialHighlightedNotes,
-    specialHighlightColor,
-    showOctaveNumbers,
-    keyPressStyle,
-    vividKeyPressColor,
+	onNoteDown,
+	onNoteUp,
+	startOctave,
+	startNote,
+	endOctave,
+	endNote,
+	showNoteNames,
+	highlightedNotes,
+	highlightColor,
+	specialHighlightedNotes,
+	specialHighlightColor,
+	showOctaveNumbers,
+	keyPressStyle,
+	vividKeyPressColor,
 }: VirtualPianoProps) => {
-    const notesList: Array<Note> = [
-        Note.C,
-        Note.D,
-        Note.E,
-        Note.F,
-        Note.G,
-        Note.A,
-        Note.B,
-    ];
-    const octaveList = [];
+	const notesList: Array<Note> = [
+		Note.C,
+		Note.D,
+		Note.E,
+		Note.F,
+		Note.G,
+		Note.A,
+		Note.B,
+	];
+	const octaveList = [];
 
-    for (let octaveNum = startOctave; octaveNum <= endOctave; octaveNum++) {
-        octaveList.push(octaveNum);
-    };
+	for (let octaveNum = startOctave; octaveNum <= endOctave; octaveNum++) {
+		octaveList.push(octaveNum);
+	}
 
 	return (
 		<Row>
-            {octaveList.map((octaveNum) => {
-                return (
-                    <Octave
-                        width={"350px"}
-                        height={"200px"}
-                        key={octaveNum}
-                        number={octaveNum}
-                        showNoteNames={showNoteNames}
-                        showOctaveNumber={showOctaveNumbers}
-                        startNote={octaveNum == startOctave ? startNote : notesList[0]}
-                        endNote={octaveNum == endOctave ? endNote : notesList[notesList.length - 1]}
-                        onNoteDown={onNoteDown}
-                        onNoteUp={onNoteUp}
-                    />
-                );
-            })}
-        </Row>
+			{octaveList.map((octaveNum) => {
+				return (
+					<Octave
+						width={"350px"}
+						height={"200px"}
+						key={octaveNum}
+						number={octaveNum}
+						showNoteNames={showNoteNames}
+						showOctaveNumber={showOctaveNumbers}
+						highlightedNotes={highlightedNotes}
+						startNote={octaveNum == startOctave ? startNote : notesList[0]}
+						endNote={
+							octaveNum == endOctave ? endNote : notesList[notesList.length - 1]
+						}
+						onNoteDown={onNoteDown}
+						onNoteUp={onNoteUp}
+					/>
+				);
+			})}
+		</Row>
 	);
 };
 
 VirtualPiano.defaultProps = {
-    onNoteDown: (n) => {
-        console.log("Note down: " + keyToStr(n));
-    },
-    onNoteUp: (n) => {
-        console.log("Note up: " + keyToStr(n));
-    },
+	onNoteDown: (n) => {
+		console.log("Note down: " + keyToStr(n));
+	},
+	onNoteUp: (n) => {
+		console.log("Note up: " + keyToStr(n));
+	},
 	startOctave: 2,
 	startNote: Note.C,
 	endOctave: 2,
 	endNote: Note.B,
 	showNoteNames: NoteNameBehavior.onpress,
-	highlightedNotes: [],
+	highlightedNotes: [{ key: Note.D }, { key: Note.G, bgColor: "blue" }],
 	highlightColor: "red",
 	specialHighlightedNotes: [Note.D, Note.G],
 	specialHighlightColor: "blue",
