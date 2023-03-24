be0e41006a4988fa3acd99069e9e95dc26471f98
GitBluub <louis.auzuret@gmail.com>
2023-03-24 21:37:08 +0900

    fix populate.py and new musics

--- musics/populate.py
+++ musics/populate.py
@@ -4,11 +4,13 @@ import sys
 import os
 import requests
 import glob
-from configparser import ConfigParser 
+from configparser import ConfigParser
 
 url = os.environ.get("API_URL")
 
 def getOrCreateAlbum(name, artistId):
+	if not name:
+		return
 	res = requests.post(f"{url}/album", json={
 		"name": name,
 		"artist": artistId,
@@ -17,13 +19,17 @@ def getOrCreateAlbum(name, artistId):
 	print(out)
 	return out["id"]
 
-def getOrCreateGenre(name):
-	res = requests.post(f"{url}/genre", json={
-		"name": name,
-	})
-	out = res.json()
-	print(out)
-	return out["id"]
+def getOrCreateGenre(names):
+    ids = []
+    for name in names.split(","):
+        res = requests.post(f"{url}/genre", json={
+    	    "name": name,
+        })
+        out = res.json()
+        print(out)
+        ids += [out["id"]]
+    #TODO handle multiple genres
+    return ids[0]
 
 def getOrCreateArtist(name):
 	res = requests.post(f"{url}/artist", json={
@@ -37,14 +43,14 @@ def populateFile(path, midi, mxl):
 	config = ConfigParser()
 	config.read(path)
 	metadata = config["Metadata"];
-	dificulties = dict(config["Difficulties"])
+	difficulties = dict(config["Difficulties"])
 	artistId = getOrCreateArtist(metadata["Artist"])
 	print(f"Populating {metadata['Name']}")
 	res = requests.post(f"{url}/song", json={
 		"name": metadata["Name"],
 		"midiPath": f"/musics/{midi}",
 		"musicXmlPath": f"/musics/{mxl}",
-		"difficulties": dificulties,
+		"difficulties": difficulties,
 		"artist": artistId,
 		"album": getOrCreateAlbum(metadata["Album"], artistId),
 		"genre": getOrCreateGenre(metadata["Genre"]),
@@ -61,6 +67,6 @@ def main():
 		print(f"File found: {file}")
 		path = os.path.splitext(file)[0]
 		populateFile(file, path + ".midi", path + ".mxl")
-	
+
 if __name__ == "__main__":
 	exit(main())
