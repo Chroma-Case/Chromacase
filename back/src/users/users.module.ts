9e9c7122631ff2d528f22984ada96e680d04e508
Quentin TREHEUX <quentin.treheux@epitech.eu>
2023-04-16 08:39:29 +0200

    [UPD] add google strategy provider

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -1,12 +1,15 @@
+// users/users.module.ts
+
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
 	imports: [PrismaModule],
 	controllers: [UsersController],
-	providers: [UsersService],
+	providers: [UsersService, GoogleStrategy],
 	exports: [UsersService],
 })
 export class UsersModule {}
