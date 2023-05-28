2838beae124a26a5ccb1df9ae395347f1bc5388a
Zoe Roux <zoe.roux@zoriya.dev>
2023-05-28 14:44:57 +0900

    Remove the unsafe user api

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,31 +1,12 @@
-import {
-	Controller,
-	Get,
-	Post,
-	Body,
-	Param,
-	Delete,
-	NotFoundException,
-} from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { SettingsService } from 'src/settings/settings.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/models/user';
-import { resolve } from 'path';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-	constructor(private readonly usersService: UsersService, private readonly settingsService: SettingsService) {}
-
-	@Post()
-	create(@Body() createUserDto: CreateUserDto): Promise<User> {
-		return this.usersService.createUser(createUserDto).then((user) => {
-			this.settingsService.createUserSetting(user.id);
-			return user;
-		}).catch((e) => e);
-	}
+	constructor(private readonly usersService: UsersService) {}
 
 	@Get()
 	findAll(): Promise<User[]> {
@@ -38,10 +19,5 @@ export class UsersController {
 		const ret = await this.usersService.user({ id: +id });
 		if (!ret) throw new NotFoundException();
 		return ret;
-	}
-
-	@Delete(':id')
-	remove(@Param('id') id: string): Promise<User> {
-		return this.usersService.deleteUser({ id: +id });
 	}
 }
