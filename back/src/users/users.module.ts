4c666ccb19a9918ad9bf9e47be40514a238a1ad2
Quentin TREHEUX <quentin.treheux@epitech.eu>
2023-05-28 11:25:50 +0200

    [UPD] add module google & auth

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -5,11 +5,26 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { GoogleStrategy } from './google.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+
 
 @Module({
-	imports: [PrismaModule],
+	imports: [
+		PrismaModule,
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				secret: configService.get('JWT_SECRET'),
+				signOptions: { expiresIn: '1d' },
+			}),
+			inject: [ConfigService],
+		}),
+	],
 	controllers: [UsersController],
-	providers: [UsersService, GoogleStrategy],
+	providers: [UsersService, GoogleStrategy, AuthService],
 	exports: [UsersService],
 })
 export class UsersModule {}
