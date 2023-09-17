b8a2f04a2ffc57bbbe46c60756d61f921dd8a04e
Clément Le Bihan <clement.lebihan773@gmail.com>
2023-09-17 19:34:11 +0200

    prettied phaserCanvas.ts

--- front/components/PartitionVisualizer/PhaserCanvas.tsx
+++ front/components/PartitionVisualizer/PhaserCanvas.tsx
@@ -75,12 +75,11 @@ const getPianoScene = (
 				if (this.nbTextureToload > 0) return;
 				this.partition = this.add.image(0, 0, 'partition').setOrigin(0, 0);
 				this.cameras.main.setBounds(0, 0, this.partition.width, this.partition.height);
-				console.log('canvas', this.partition.width, this.partition.height);
 
 				const dims = this.partition.getBounds();
 				// base ref normal cursor is 350px by 30px
 				this.cursor = this.add
-					.rectangle(0, 0, dims.height * 30 / 276, dims.height, 0x31ef8c, 0.5)
+					.rectangle(0, 0, (dims.height * 30) / 276, dims.height, 0x31ef8c, 0.5)
 					.setOrigin(0, 0);
 				this.cameras.main.startFollow(this.cursor, true, 0.05, 0.05);
 
