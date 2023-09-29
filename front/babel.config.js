b4f268dee0003ce2dd6c6fc43ccea883c75fb40d
Arthur Jamet <arthur.jamet@gmail.com>
2023-09-29 15:47:16 +0200

    Front: Redump Expo

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,12 +1,12 @@
-module.exports = function (api) {
+module.exports = function(api) {
 	api.cache(true);
 	return {
 		presets: ['babel-preset-expo'],
-		plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin'],
-		env: {
-			production: {
-				plugins: ['react-native-paper/babel'],
-			},
-		},
+		// plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin'],
+		// env: {
+		// 	production: {
+		// 		plugins: ['react-native-paper/babel'],
+		// 	},
+		// },
 	};
 };
