b62141fa9ee8f262bbd56f60ae245df14ea818c1
Arthur Jamet <arthur.jamet@gmail.com>
2023-10-22 11:39:42 +0200

    Front: Make Metronome Loading sound compatible

--- front/components/Metronome.tsx
+++ front/components/Metronome.tsx
@@ -1,21 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { useEffect, useRef } from 'react';
 import { Slider, Switch, Text, View } from 'native-base';
+import { Audio } from 'expo-av';
 
 export const Metronome = ({ paused = false, bpm }: { paused?: boolean; bpm: number }) => {
-	const ref = useRef<HTMLAudioElement | null>(null);
+	const audio = useRef<Audio.Sound | null>(null);
 	const enabled = useRef<boolean>(false);
 	const volume = useRef<number>(50);
 
 	useEffect(() => {
+		if (!enabled) {
+			return;
+		} else if (!audio.current) {
+			Audio.Sound.createAsync(require('../assets/metronome.mp3'))
+			.then((a) => {
+				audio.current = a.sound;
+			})
+		}
+		return () => {
+			audio.current?.unloadAsync();
+		}
+	}, [enabled]);
+	useEffect(() => {
 		if (paused) return;
 		const int = setInterval(() => {
 			if (!enabled.current) return;
-			if (!ref.current) ref.current = new Audio('/assets/metronome.mp3');
-			ref.current.volume = volume.current / 100;
-			ref.current.play();
+			if (!audio.current) return;
+			audio.current?.playAsync();
 		}, 60000 / bpm);
 		return () => clearInterval(int);
 	}, [bpm, paused]);
+
+	useEffect(() => {
+		audio.current?.setVolumeAsync(volume.current / 100);
+	}, [volume.current]);
 	return (
 		<View>
 			<Text>Metronome Settings</Text>
