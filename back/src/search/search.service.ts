ca390200d950d782d29803ae46afc8d4e818ec65
Zoe Roux <zoe.roux@zoriya.dev>
2023-10-27 23:01:36 +0200

    Implement search controler

--- back/src/search/search.service.ts
+++ back/src/search/search.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { Artist, Prisma, Song, Genre } from '@prisma/client';
-import { HistoryService } from 'src/history/history.service';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { MeiliService } from './meilisearch.service';
+import { Injectable } from "@nestjs/common";
+import { Artist, Prisma, Song, Genre } from "@prisma/client";
+import { HistoryService } from "src/history/history.service";
+import { PrismaService } from "src/prisma/prisma.service";
+import { MeiliService } from "./meilisearch.service";
 
 @Injectable()
 export class SearchService {
@@ -12,42 +12,68 @@ export class SearchService {
 		private search: MeiliService,
 	) {}
 
-	async searchSong(query: string, artistId?: number): Promise<Song[]> {
+	async searchSong(
+		query: string,
+		artistId?: number,
+		include?: Prisma.SongInclude,
+		skip?: number,
+		take?: number,
+	): Promise<Song[]> {
 		if (query.length === 0) {
 			return await this.prisma.song.findMany({
 				where: {
 					artistId,
 				},
+				take,
+				skip,
+				include,
 			});
 		}
-		return (await this.search
-			.index('songs')
-			.search(query, { filter: `artistId = ${artistId}` })) as any;
+		const ids = (
+			await this.search.index("songs").search(query, {
+				limit: take,
+				offset: skip,
+				...(artistId ? { filter: `artistId = ${artistId}` } : {}),
+			})
+		).hits.map((x) => x.id);
+
+		return (
+			await this.prisma.song.findMany({
+				where: {
+					id: { in: ids },
+				},
+				include,
+			})
+		).sort((x) => ids.indexOf(x.id));
 	}
 
-	async genreByGuess(
+	async searchArtists(
 		query: string,
-		userID: number,
-		include?: Prisma.GenreInclude,
-	): Promise<Genre[]> {
-		return this.prisma.genre.findMany({
-			where: {
-				name: { contains: query, mode: "insensitive" },
-			},
-			include,
-		});
-	}
-
-	async artistByGuess(
-		query: string,
-		userID: number,
 		include?: Prisma.ArtistInclude,
+		skip?: number,
+		take?: number,
 	): Promise<Artist[]> {
-		return this.prisma.artist.findMany({
-			where: {
-				name: { contains: query, mode: "insensitive" },
-			},
-			include,
-		});
+		if (query.length === 0) {
+			return this.prisma.artist.findMany({
+				take,
+				skip,
+				include,
+			});
+		}
+		const ids = (
+			await this.search.index("artists").search(query, {
+				limit: take,
+				offset: skip,
+			})
+		).hits.map((x) => x.id);
+
+		return (
+			await this.prisma.artist.findMany({
+				where: {
+					id: { in: ids },
+				},
+				include,
+			})
+		).sort((x) => ids.indexOf(x.id));
 	}
 }
