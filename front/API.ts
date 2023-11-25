8fa921a2cde9ea2b316ddcd581fc7b6558190616
Clément Le Bihan <clement.lebihan773@gmail.com>
2023-11-25 01:39:47 +0100

    Added must wondition for APIBase Url for dev modes

--- front/API.ts
+++ front/API.ts
@@ -67,7 +67,7 @@ export class ValidationError extends Error {
 
 export default class API {
 	public static readonly baseUrl =
-		Platform.OS === 'web' ? '/api' : process.env.EXPO_PUBLIC_API_URL!;
+		Platform.OS === 'web' && !process.env.EXPO_PUBLIC_API_URL ? '/api' : process.env.EXPO_PUBLIC_API_URL!;
 	public static async fetch(
 		params: FetchParams,
 		handle: Pick<Required<HandleParams>, 'raw'>
