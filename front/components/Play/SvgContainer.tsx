a15f2378e67d9c2693f62728a996bbeadaf3fe08
Clément Le Bihan <clement.lebihan773@gmail.com>
2023-11-27 23:11:37 +0100

    now using dims from cursor file info to set correct ratios for the partition display independantly of the front end platform

--- front/components/Play/SvgContainer.tsx
+++ front/components/Play/SvgContainer.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { ViewStyle, ImageBackground, Platform, Image } from 'react-native';
+import { ViewStyle, ImageBackground, Platform } from 'react-native';
 import { SvgCssUri } from 'react-native-svg/css';
 
 type SvgContainerProps = {
 	url: string;
 	onReady?: () => void;
 	style?: ViewStyle;
-};
-
-export const GetSvgDims = (url: string, success: (w: number, h: number) => void) => {
-	if (Platform.OS === 'web') {
-		React.useEffect(() => {
-			Image.getSize(url, (w, h) => {
-				success(w, h);
-			});
-		}, [url]);
-		return;
-	}
-	// success(470.1052279999999, 275);
-	success(6146.845969199998, 408.00000000000006);
 };
 
 export const SvgContainer = ({ url, onReady, style }: SvgContainerProps) => {
