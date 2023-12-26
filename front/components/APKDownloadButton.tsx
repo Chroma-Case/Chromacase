f07eb2658b12b8f086716518d24819f0513eb959
Arthur Jamet <arthur.jamet@gmail.com>
2023-12-26 20:03:07 +0100

    Front: Explain how to DL the APK

--- front/components/APKDownloadButton.tsx
+++ front/components/APKDownloadButton.tsx
@@ -2,18 +2,36 @@ import { ArrowCircleDown2 } from 'iconsax-react-native';
 import ButtonBase from './UI/ButtonBase';
 import { translate } from '../i18n/i18n';
 import { Linking } from 'react-native';
+import { useState } from 'react';
+import PopupCC from './UI/PopupCC';
 
 const APKDownloadButton = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	// 
 	return (
-		<ButtonBase
-			style={{}}
-			icon={ArrowCircleDown2}
-			type={'filled'}
-			title={translate('downloadAPK')}
-			onPress={() =>
-				Linking.openURL('https://github.com/Chroma-Case/Chromacase/releases/download/v0.8.4/android-build.apk')
-			}
-		/>
+		<>
+			<ButtonBase
+				style={{}}
+				icon={ArrowCircleDown2}
+				type={'filled'}
+				title={translate('downloadAPK')}
+				onPress={() => setIsOpen(true)}
+			/>
+			<PopupCC
+				title={translate('downloadAPK')}
+				description={translate('downloadAPKInstructions')}
+				isVisible={isOpen}
+				setIsVisible={setIsOpen}
+			>
+				<ButtonBase
+					style={{}}
+					icon={ArrowCircleDown2}
+					type={'filled'}
+					title={translate('downloadAPK')}
+					onPress={() => Linking.openURL('https://github.com/Chroma-Case/Chromacase/releases')}
+				/>	
+			</PopupCC>
+		</>
 	);
 };
 
