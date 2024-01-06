b1727b783873fb8f2b3d44ab090728e61a1848a8
Zoe Roux <zoe.roux@zoriya.dev>
2024-01-06 12:24:51 +0100

    Fix gradient in white mode

--- front/Navigation.tsx
+++ front/Navigation.tsx
@@ -178,7 +178,7 @@ const RouteToScreen = <T extends {}>(Component: Route<T>['component']) =>
 
 		return (
 			<LinearGradient
-				colors={colorScheme === 'dark' ? ['#101014', '#6075F9'] : ['#cdd4fd']}
+				colors={colorScheme === 'dark' ? ['#101014', '#6075F9'] : ['#cdd4fd', "#cdd4fd"]}
 				style={{
 					width: '100%',
 					height: '100%',
